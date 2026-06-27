ac226f21911766b90ba73b17e1c56d66d283c0a9
TanjiroKamado1XXX <namankumar.1207@gmail.com>
2026-06-27 16:41:53 +0530

    Change guildId to null in application configuration
    Set guildId to null instead of using environment variable.

--- src/config/application.js
+++ src/config/application.js
@@ -22,7 +22,7 @@ const appConfig = {
     ...botConfig,
     token: process.env.DISCORD_TOKEN || process.env.TOKEN,
     clientId: process.env.CLIENT_ID,
-    guildId: process.env.GUILD_ID,
+    guildId: null,,
     multiGuild: process.env.MULTI_GUILD === 'true',
 
     shop: {
